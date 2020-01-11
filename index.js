0309ba14b1d8b991ed69313cf6a339f561292253
Luke Hoel <hoel.luke@gmail.com>
2020-01-11 11:05:09 -0500

    got effect generation working

--- index.js
+++ index.js
@@ -25,37 +25,65 @@ const removeAction = (id) => {
     actions.splice(index, 1);
 };
 
-const parseActionEnum = (action) => {
+const setActionsData = () => {
     const groupName = document.getElementById("groupName").value;
+    const groupNameFirstLower = `${groupName[0].toLowerCase()}${groupName.substring(1)}`
+    return actions.map(action => {
+        const name = action.querySelector("#name").value;
+        const actionName = `${name[0].toUpperCase()}${name.substring(1)}${groupName}`
+        const description = action.querySelector("#description").value;
+        return { groupName, groupNameFirstLower, name, actionName, description };
+    });
+}
 
-    const name = action.querySelector("#name").value;
+const generateActionEnum = ({ groupName, groupNameFirstLower, name, actionName, description }) => {
     // Enum naming convention
-    const actionName = `${name[0].toUpperCase()}${name.substring(1)}${groupName}`
-
-    const description = action.querySelector("#description").value;
     return `${actionName} = '[${groupName}] ${description}',
             ${actionName}Success = '[${groupName}] ${description} Success'`;
 }
 
-const parseAction = (action) => {
-    const groupName = document.getElementById("groupName").value;
-
-    const name = action.querySelector("#name").value;
+const generateAction = ({ groupName, groupNameFirstLower, name, actionName, description }) => {
     // Enum naming convention
-    const actionName = `${name[0].toUpperCase()}${name.substring(1)}${groupName}`
     return `export const ${name}${groupName} = createAction(${groupName}Actions.${actionName}, props<{}>());
     export const ${name}${groupName}Success = createAction(${groupName}Actions.${actionName}Success, props<{}>());`;
 }
 
+const generateEffect = ({ groupName, groupNameFirstLower, name, actionName, description }) => {
+    return `
+        ${name}${groupName}$ = createEffect(() =>
+            this.actions$.pipe(
+            ofType(${name}${groupName}Action),
+            switchMap((action: any) =>
+                this.${groupNameFirstLower}Service.${name}${groupName}().pipe(
+                map(() => ${name}${groupName}SuccessAction()),
+                catchError(() => EMPTY)
+                )
+            )
+            )
+        );
+    `
+};
+
 const generate = (event) => {
     document.getElementById("results").style.display = "block";
+    const actionsData = setActionsData();
 
     const groupName = document.getElementById("groupName").value;
+    const groupNameFirstLower = `${groupName[0].toLowerCase()}${groupName.substring(1)}`
 
+    // Set actions
     document.getElementById("action").innerText = `
-    export enum ${groupName}Actions {
-      ${actions.map(parseActionEnum).join(',')}
-    }
-    ${actions.map(parseAction).join('')}`;
+        export enum ${groupName}Actions {
+        ${actionsData.map(generateActionEnum).join(',')}
+        }
+    ${actionsData.map(generateAction).join('')}`;
 
+    // Set effects
+    document.getElementById("effect").innerText = `
+        @Injectable()
+        export class ${groupName}Effect {
+            ${actionsData.map(generateEffect).join('')}
+        constructor(private actions$: Actions, private ${groupNameFirstLower}Service: ${groupName}Service) {}
+        }
+    `;
 };

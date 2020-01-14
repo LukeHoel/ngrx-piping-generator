a4b1b9a97bc5ae4ab47f8320e3a99a0f8696f7ab
Luke Hoel <hoel.luke@gmail.com>
2020-01-13 20:09:35 -0500

    naming change

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const addAction = () => {
     action.innerHTML = `
         <hr>
         <div>
-            <label>Name</label>
+            <label>Action Name</label>
             <input id="name" type="text" value="">
         </div>
         <div>
-            <label>Description</label>
+            <label>Action Description</label>
             <input id="description" type="text" value="">
         </div>
         <button class="buttonRemove buttonSecondary" onclick="removeAction(${actionId})">Remove Action</button>
@@ -28,45 +28,45 @@ const removeAction = (id) => {
 };
 
 const setActionsData = () => {
-    const groupNameRaw = document.getElementById("groupName").value;
-    if(!groupNameRaw){
-	return [];
+    const featureNameRaw = document.getElementById("featureName").value;
+    if (!featureNameRaw) {
+        return [];
     }
-    const groupName = pascalCase(groupNameRaw);
-    const groupNameFirstLower = `${groupName[0].toLowerCase()}${groupName.substring(1)}`
+    const featureName = pascalCase(featureNameRaw);
+    const featureNameFirstLower = `${featureName[0].toLowerCase()}${featureName.substring(1)}`
     return actions.map(action => {
         const name = _.camelCase(action.querySelector("#name").value);
         const description = action.querySelector("#description").value;
         if (!name || !description) {
             return;
         }
-        const actionName = `${name[0].toUpperCase()}${name.substring(1)}${groupName}`
-        return { groupName, groupNameFirstLower, name, actionName, description };
+        const actionName = `${name[0].toUpperCase()}${name.substring(1)}${featureName}`
+        return { featureName, featureNameFirstLower, name, actionName, description };
     }).filter(action => action);
 }
 
-const generateActionEnum = ({ groupName, groupNameFirstLower, name, actionName, description }) => {
+const generateActionEnum = ({ featureName, featureNameFirstLower, name, actionName, description }) => {
     // Enum naming convention
-    return `${actionName} = '[${groupName} component] ${description}',
-            ${actionName}Success = '[${groupName} component] ${description} Success'`;
+    return `${actionName} = '[${featureName} component] ${description}',
+            ${actionName}Success = '[${featureName} component] ${description} Success'`;
 }
 
-const generateAction = ({ groupName, groupNameFirstLower, name, actionName, description }) => {
+const generateAction = ({ featureName, featureNameFirstLower, name, actionName, description }) => {
     // Enum naming convention
-    return `export const ${name}${groupName}Action = createAction(${groupName}Actions.${actionName}, props<{}>());
-    export const ${name}${groupName}SuccessAction = createAction(${groupName}Actions.${actionName}Success, props<{}>());`;
+    return `export const ${name}${featureName}Action = createAction(${featureName}Actions.${actionName}, props<{}>());
+    export const ${name}${featureName}SuccessAction = createAction(${featureName}Actions.${actionName}Success, props<{}>());`;
 }
 
-const generateActionImports = ({ groupName, groupNameFirstLower, name, actionName, description }) => `${name}${groupName}Action, ${name}${groupName}SuccessAction`;
+const generateActionImports = ({ featureName, featureNameFirstLower, name, actionName, description }) => `${name}${featureName}Action, ${name}${featureName}SuccessAction`;
 
-const generateEffect = ({ groupName, groupNameFirstLower, name, actionName, description }) => {
+const generateEffect = ({ featureName, featureNameFirstLower, name, actionName, description }) => {
     return `
-        ${name}${groupName}$ = createEffect(() =>
+        ${name}${featureName}$ = createEffect(() =>
             this.actions$.pipe(
-            ofType(${name}${groupName}Action),
+            ofType(${name}${featureName}Action),
             switchMap((action: any) =>
-                this.${groupNameFirstLower}Service.${name}${groupName}().pipe(
-                map(() => ${name}${groupName}SuccessAction({})),
+                this.${featureNameFirstLower}Service.${name}${featureName}().pipe(
+                map(() => ${name}${featureName}SuccessAction({})),
                 catchError(() => EMPTY)
                 )
             )
@@ -75,17 +75,17 @@ const generateEffect = ({ groupName, groupNameFirstLower, name, actionName, desc
     `
 };
 
-const generateService = ({ groupName, groupNameFirstLower, name, actionName, description }) => {
-    return `${name}${groupName}(): Observable<any> {
+const generateService = ({ featureName, featureNameFirstLower, name, actionName, description }) => {
+    return `${name}${featureName}(): Observable<any> {
                 return null;
             }`;
 }
 
-const generateStateImports = ({ groupName, groupNameFirstLower, name, actionName, description }) => `I${groupName}State, Initial${groupName}State`;
+const generateStateImports = ({ featureName, featureNameFirstLower, name, actionName, description }) => `I${featureName}State, Initial${featureName}State`;
 
-const generateReducer = ({ groupName, groupNameFirstLower, name, actionName, description }) => {
-    return `on(${name}${groupName}Action, state => ({ ...state })),
-    on(${name}${groupName}SuccessAction, state => ({ ...state }))
+const generateReducer = ({ featureName, featureNameFirstLower, name, actionName, description }) => {
+    return `on(${name}${featureName}Action, state => ({ ...state })),
+    on(${name}${featureName}SuccessAction, state => ({ ...state }))
     `;
 };
 
@@ -97,13 +97,13 @@ const generate = (event) => {
         return;
     }
     document.getElementById("results").style.display = "block";
-    const groupName = pascalCase(document.getElementById("groupName").value);
-    const groupNameFirstLower = `${groupName[0].toLowerCase()}${groupName.substring(1)}`
+    const featureName = pascalCase(document.getElementById("featureName").value);
+    const featureNameFirstLower = `${featureName[0].toLowerCase()}${featureName.substring(1)}`
 
     // Set actions
     document.getElementById("action").innerText = js_beautify(`
         import { createAction, props } from '@ngrx/store';
-        export enum ${groupName}Actions {
+        export enum ${featureName}Actions {
         ${actionsData.map(generateActionEnum).join(',')}
         }
     ${actionsData.map(generateAction).join('')}`);
@@ -114,11 +114,11 @@ const generate = (event) => {
         import { createEffect, ofType, Actions } from '@ngrx/effects';
         import { switchMap, map, catchError } from 'rxjs/operators';
         import { EMPTY } from 'rxjs';
-        import { ${actionsData.map(generateActionImports).join(',')} } from '../actions/${_.kebabCase(groupName)}.action';
+        import { ${actionsData.map(generateActionImports).join(',')} } from '../actions/${_.kebabCase(featureName)}.action';
         @Injectable()
-        export class ${groupName}Effect {
+        export class ${featureName}Effect {
             ${actionsData.map(generateEffect).join('')}
-        constructor(private actions$: Actions, private ${groupNameFirstLower}Service: ${groupName}Service) {}
+        constructor(private actions$: Actions, private ${featureNameFirstLower}Service: ${featureName}Service) {}
         }
     `);
 
@@ -130,7 +130,7 @@ const generate = (event) => {
         @Injectable({
             providedIn: 'root'
         })
-        export class ${groupName}Service {
+        export class ${featureName}Service {
             constructor(private http: HttpClient) {}
             ${actionsData.map(generateService).join('')}
         }`);
@@ -138,26 +138,26 @@ const generate = (event) => {
     // Set reducers
     document.getElementById('reducer').innerText = js_beautify(`
       import { createReducer, on, Action } from '@ngrx/store';
-      import { ${actionsData.map(generateActionImports).join(',')} } from '../actions/${_.kebabCase(groupName)}.action';
-      import { ${actionsData.map(generateStateImports).join(',')} } from '../states/${_.kebabCase(groupName)}.state';
-      const ${groupNameFirstLower}Reducers = createReducer(
-        Initial${groupName}State,
+      import { ${actionsData.map(generateActionImports).join(',')} } from '../actions/${_.kebabCase(featureName)}.action';
+      import { ${actionsData.map(generateStateImports).join(',')} } from '../states/${_.kebabCase(featureName)}.state';
+      const ${featureNameFirstLower}Reducers = createReducer(
+        Initial${featureName}State,
         ${actionsData.map(generateReducer).join(',')}
       );
       
-      export function ${groupName}Reducer(state: I${groupName}State, action: Action) {
-        return ${groupNameFirstLower}Reducers(state, action);
+      export function ${featureName}Reducer(state: I${featureName}State, action: Action) {
+        return ${featureNameFirstLower}Reducers(state, action);
       }
     `);
 
     // Set state
     document.getElementById('state').innerText = js_beautify(`
-        export interface I${groupName}State {}
-        export const Initial${groupName}State: I${groupName}State = {};
+        export interface I${featureName}State {}
+        export const Initial${featureName}State: I${featureName}State = {};
     `);
 
     // Set header file names
-    const fileName = (type) => `${_.kebabCase(groupName)}.${type}.ts`;
+    const fileName = (type) => `${_.kebabCase(featureName)}.${type}.ts`;
     document.getElementById("actionFileName").innerText = fileName("action");
     document.getElementById("effectFileName").innerText = fileName("effect");
     document.getElementById("serviceFileName").innerText = fileName("service");
